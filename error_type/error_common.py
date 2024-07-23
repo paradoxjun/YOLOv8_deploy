cb1e4719ae3219a7f8203e849afe69250d846fad
paradoxjun <59300509+paradoxjun@users.noreply.github.com>
2024-07-23 14:40:45 +0800

    Add files via upload

---
+++ error_type/error_common.py
@@ -0,0 +1,26 @@
+class ParsingUrlError(Exception):
+    pass
+
+
+class ReadImageError(Exception):
+    pass
+
+
+class InvalidImageError(Exception):
+    pass
+
+
+class InputFormatError(Exception):
+    pass
+
+
+class PreProcessError(Exception):
+    pass
+
+
+class DetectionInferError(Exception):
+    pass
+
+
+class PostProcessError(Exception):
+    pass
